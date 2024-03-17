b94a4e9ea78da6207be28a72fc2e3618abe2310e
David Bankole <bankoledavidanjola@gmail.com>
2024-03-17 15:37:54 -0700

    Task 3 excluding letters from ASCII alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+print(
+    ''.join(
+        chr(97 + i) for i in range(26) if chr(97 + i) not in ['q', 'e']
+    ).format(),
+    end=''
+)
