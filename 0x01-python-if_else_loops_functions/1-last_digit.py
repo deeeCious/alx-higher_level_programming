f54653a4b5ab382876f3b7dfda406da83d5fa568
David Bankole <bankoledavidanjola@gmail.com>
2024-03-17 15:04:30 -0700

    Task 2 last digit correction from my local machine

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
 # Extracting the last digit of the number
 if number < 0:
     last_digit = number % -10
@@ -9,8 +8,8 @@ else:
     last_digit = number % 10
 # Check if last digit is greater than 5
 if last_digit > 5:
-    print("Last digit of {number} and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print("Last digit of {number} and is 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print("Last digit of {number} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
